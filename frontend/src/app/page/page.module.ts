eba03af51b60bc47bfe52f781395bcec6107e55b
jason zheng <101104044@nccu.edu.tw>
2019-08-22 00:20:24 +0800

    [frontend] add all task component

--- frontend/src/app/page/page.module.ts
+++ frontend/src/app/page/page.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageRoutungModule } from './page-routung.module';
+import { AllTasksComponent } from './components/all-tasks/all-tasks.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [AllTasksComponent],
   imports: [
     CommonModule,
     PageRoutungModule
