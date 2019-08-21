f7a19b4020e832d33af1e16d65cc7fc67a0b7bbe
jason zheng <101104044@nccu.edu.tw>
2019-08-22 00:04:56 +0800

    [frontend] add page module

---
+++ frontend/src/app/page/page.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PageRoutungModule } from './page-routung.module';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    PageRoutungModule
+  ]
+})
+export class PageModule { }
